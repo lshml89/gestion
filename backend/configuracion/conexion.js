cbaa936b2bdcef3ee2f90f21ce9a1b34df19bbfb
erick limache <ericklimache14@gmail.com>
2026-05-06 22:51:07 -0600

    Fix SSL MySQL

--- backend/configuracion/conexion.js
+++ backend/configuracion/conexion.js
@@ -1,20 +1,22 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
 const conexion = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 conexion.connect((error) => {
   if (error) {
     console.log("Error al conectar con MySQL:", error);
-    return;
+  } else {
+    console.log("Conectado a MySQL 🚀");
   }
-
-  console.log("Conectado correctamente a MySQL");
 });
 
 module.exports = conexion;
